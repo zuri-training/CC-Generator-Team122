adce047ab560f7a44a77479c72379fb4991eb073
Ayomide <105179622+MideMario@users.noreply.github.com>
2022-08-09 15:12:39 +0100

    created tutorials.html page, created django url for all views and linked all views to their templates

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, about_us
+from .views import home, about_us, contact_us, landingpage, library, profile, documentation, api_ref
 
 
 
@@ -7,12 +7,12 @@ from .views import home, about_us
 
 urlpatterns = [
     path('', home.as_view(), name='home'),
-    path('about_us', about_us.as_view(), name='about_us')
-    # path('contact_us', views.contact_us, name='contact_us')
-    # path('library', views.library, name='library')
-    # path('profile', profile, name='profile')
-    # path('settings', view.settings, name='settings')
-    # path('documentation', view.documentation, name='documentation')
-    # path('api_ref', view.api_ref, name='api_ref')
+    path('about_us', about_us.as_view(), name='about_us'),
+    path('contact_us', contact_us.as_view(), name='contact_us'),
+    path('landingpage', landingpage.as_view(), name='landingpage'),
+    path('library', library.as_view(), name='library'),
+    path('profile', profile.as_view(), name='profile'),
+    path('documentation', documentation.as_view(), name='documentation'),
+    path('api_ref', api_ref.as_view(), name='api_ref'),
      ]
 
--- core/views.py
+++ core/views.py
@@ -6,3 +6,15 @@ class home(generic.TemplateView) :
     template_name = "home.html"
 class about_us(generic.TemplateView) :
     template_name = "about_us.html"
+class contact_us(generic.TemplateView) :
+    template_name = "contact_us.html"
+class landingpage(generic.TemplateView) :
+    template_name = "landingpage.html"
+class library(generic.TemplateView) :
+    template_name = "library.html"
+class profile(generic.TemplateView) :
+    template_name = "userprofile.html"
+class documentation(generic.TemplateView) :
+    template_name = "tutorials.html"
+class api_ref(generic.TemplateView) :
+    template_name = "apidoc.html"
